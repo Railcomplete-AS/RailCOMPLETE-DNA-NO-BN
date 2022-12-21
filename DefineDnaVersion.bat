405c0309d1442df9959567d434f79e9681251b15
RailCOMPLETE <clfey@railcomplete.no>
2022-12-21 01:35:23 +0100

    Changed name for upcoming release from 2022.a into 2023.a

--- DefineDnaVersion.bat
+++ DefineDnaVersion.bat
@@ -7,7 +7,7 @@ rem Set TUTORIALS to no or yes to decide if copying the tutorials files is neede
 rem Set CLEAN to no or yes to decide if existing files in target folders shall be deleted.
 
 set       ADM=NO-BN
-set   RELEASE=2022.a
+set   RELEASE=2023.a
 set       LOG=no
 set    COPY3D=no
 set TUTORIALS=yes
