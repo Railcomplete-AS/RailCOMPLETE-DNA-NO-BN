a452c9e834d0c4c0e090d08d9517f5ce73d41345
khhelland <knut.helland@gmail.com>
2025-02-06 15:18:47 +0100

    script now handles kof, simple kof and excel with key or proximity based object location

--- NO-BN/Lua/Scripts/Adjust object locations to XY(Z)/2025-02-05_000 Adjust object to XY(Z) coordinates.lua
+++ NO-BN/Lua/Scripts/Adjust object locations to XY(Z)/2025-02-05_000 Adjust object to XY(Z) coordinates.lua
@@ -1,4 +1,4 @@
-function writeln(t) write((t or "").."\n") end
+function writeln(t, symbol) write((t or "").."\n", symbol and symbol or _noSymbol) end
 function show(t) writeln(t) askForKeyword(t, {"OK"}) end
 function parseStrictKOF(lines)
 	local result = {}
@@ -36,7 +36,7 @@ function parseStrictKOF(lines)
 				table.insert(result, record)
 				writeln("Data block 05, read data as { key: "..record.key..", X: "..record.X..", Y: "..record.Y..", Z: "..record.Z.."}")
 			else
-				writeln("Data block type "..line:sub(2,3).." is not implemented. Skipping line "..i)
+				writeln("Data block type "..line:sub(2,3).." is not implemented. Skipping line "..i, _warning)
 			end
 		else
 			writeln("Comment line, no data")
@@ -58,33 +58,33 @@ function parseSimpleKOF(lines)
 			end
 
 
-			if field[1] == "00" then
+			if fields[1] == "00" then
 				writeln("Datablock 00, no data")
-			elseif field[1] == "01" then
+			elseif fields[1] == "01" then
 				enheter.retning = tonumber(line:sub(45,45))			
 				enheter.vinkler = tonumber(line:sub(46,46))
 				enheter.avstand = tonumber(line:sub(47,47))
 				writeln("Datablock 01, changed units to {retning: "..enheter.retning..", vinkler: "..enheter.vinkler..", avstand: "..enheter.avstand.."}")				
-			elseif field[1] == "05" then
+			elseif fields[1] == "05" then
 				local record = {}
-				record.key = field[2]:gsub("%s+", "")
+				record.key = fields[2]:gsub("%s+", "")
 				local scale = 1
 				if enheter.avstand == 2 then
 					scale = 3.280839895 -- scale feet to meters
 				end
 				
 				if enheter.retning == 1 then
-					record.Y = tonumber(field[3])*scale
-					record.X = tonumber(field[4])*scale
+					record.Y = tonumber(fields[4])*scale
+					record.X = tonumber(fields[5])*scale
 				elseif enheter.retning == 2 then
-					record.X = tonumber(field[3])*scale
-					record.Y = tonumber(field[4])*scale
+					record.X = tonumber(fields[4])*scale
+					record.Y = tonumber(fields[5])*scale
 				end			
-				record.Z = tonumber(field[5])*scale
+				record.Z = tonumber(fields[6])*scale
 				table.insert(result, record)
 				writeln("Data block 05, read data as { key: "..record.key..", X: "..record.X..", Y: "..record.Y..", Z: "..record.Z.."}")
 			else
-				writeln("Data block type "..field[1].." is not implemented. Skipping line "..i)
+				writeln("Data block type "..fields[1].." is not implemented. Skipping line "..i, _warning)
 			end
 		else
 			writeln("Comment line, no data")
@@ -92,40 +92,8 @@ function parseSimpleKOF(lines)
 	end
 	return result
 end
-write(table.tostring({x=1,y=2}))
-local c = 1
---[[
-2 kof formater
-logging i parsing
-feilsjekking kill script
-by code, by name, by proximity
-]]
---[=[show([[
-	Adjust object at XY(Z) coordinates from Kof
-	=============================================
-	2025-02-04_000 KNHEL Created (from similar script adjusting objects from Excel).
-	
-	Input:
-	-	Script is run inside a RailCOMPLETE model based on any DNA, running under RC 2024.2 or later.
-	-	Kof file with XY(Z) survey points.
-	-	Ensure your model represents the same coordinate system as your XY(Z) survey points are referencing.
-	-	Use Edit Script and enable the Log output window to see more info from the execution.
-	
-	Usage:
-	-	Select kof coordinate file, select an existing RC object (to get its RcType), input the radius around
-		the coordinates to search for objects,  start the insertion.
-	-	Any DNA-formula on Mileage, ReferenceMileage, DistanceAlong, DistanceToAlingment or LateralOffset will be
-		replaced by the coordinate data.
-	-	Any DNA-formula on VerticalOffset property will be replaced by the difference in elevation between the
-		closest relevant alignment (railway tracks for most objects, contact wire for CW insulators etc) and then
-		Z elevation from the Excel file (if Z is provided).
-	-	If no 'Z' column exists in Excel, then default formulas and values apply for the VerticalOffset property.
-	
-	Output:
-	-	For each valid row in the coordinate file the closest RC object of the same RcType as the selected object
-		and is within the tolerance in 2D distance is adjusted to the coordinates.
-	]])
-	
+
+
 show([[
 Adjust object at XY(Z) coordinates from Excel
 =============================================
@@ -133,32 +101,41 @@ Adjust object at XY(Z) coordinates from Excel
 
 Input:
 -	Script is run inside a RailCOMPLETE model based on any DNA, running under RC 2024.2 or later.
+-	The source data can come from an Excel file a KOF 2.0 file or a 'simple KOF' file.
 -	Excel file with captions 'X'=Easting and 'Y'=Northing coordinates in the top row (the insertion point coords).
 -	The Excel file may contain an additional column 'Z' providing insertion point elevation above mean sea level.
+-	The Excel file may contain a column which matches either 'name', 'code' or 'id' of the objects.
 -	Subsequent rows contain X and Y coordinates (and Z) in the 'X' and the 'Y' (and Z) columns.
--	Ensure your model represents the same coordinate system as your XY(Z) survey points are referencing.
 -	Close the Excel file before running the script.
+-	For both KOF 2.0 or simple KOF only datablocks 01 and 05 are read.
+-	For KOF 2.0 the 'Punkt' field is read as keydata
+-	For Simple KOF the second non-whitespace block is read as keydata
+-	For both KOF formats scale and northing easting configurations from 01 blocks are respected
+-	Ensure your model represents the same coordinate system as your XY(Z) survey points are referencing.
 -	Use Edit Script and enable the Log output window to see more info from the execution.
 
 Usage:
--	Select Excel coordinate file, select an existing RC object (to get its RcType), input the radius around
-	the coordinates to search for objects,  start the insertion.
--	Any DNA-formula on Mileage, ReferenceMileage, DistanceAlong, DistanceToAlingment or LateralOffset will be
+-	Select file format and the coordinate file, the file is then read.
+-	Select whether to find objects based on key or proximity and select either the RC property to match with or
+	input the radius around	the coordinates to search for objects.
+-	Select an existing RC object to get its RcType.
+-	The script starts searching for objects and adjusting their position.
+-	Any formula on Mileage, ReferenceMileage, DistanceAlong, DistanceToAlingment or LateralOffset will be
 	replaced by the coordinate data.
--	Any DNA-formula on VerticalOffset property will be replaced by the difference in elevation between the
-	closest relevant alignment (railway tracks for most objects, contact wire for CW insulators etc) and then
-	Z elevation from the Excel file (if Z is provided).
--	If no 'Z' column exists in Excel, then default formulas and values apply for the VerticalOffset property.
+-	Any formula on VerticalOffset property will be replaced by the difference in elevation between the object's
+	own alignment (railway tracks for most objects, contact wire for CW insulators etc) and then Z elevation from 
+	the coordinate file (if Z is provided).
+-	If no 'Z' column exists in the coordinate file, then default formulas and values apply for the VerticalOffset property.
 
 Output:
--	For each valid row in the coordinate file the closest RC object of the same RcType as the selected object
-	and is within the tolerance in 2D distance is adjusted to the coordinates.
+-	For each valid row in the coordinate file the best match RC object of the same RcType as the selected object
+	is adjusted to the coordinates.
 ]])
-]=]
+
 
 local items
 
-fileFormat = askForKeyword("Select input file type", {"Excel", "KOF 2.0", "Simple KOF"})
+local fileFormat = askForKeyword("Select input file type", {"Excel", "KOF 2.0", "Simple KOF"})
 
 if fileFormat == "Excel" then
 	--Open Excel file:
@@ -167,7 +144,7 @@ if fileFormat == "Excel" then
 	local sheets = getExpandoObjectPropertyNames(file)
 	local sheetName = sheets[0]
 	items = table.select(file[sheetName])
-	show(nItems.." rows found in sheet "..sheetName.." in Excel file "..filename)
+	show(#items.." rows found in sheet "..sheetName.." in Excel file "..filename)
 
 else	
 	--Open Kof file:
@@ -184,24 +161,44 @@ else
 	elseif fileFormat == "Simple KOF" then
 		items = parseSimpleKOF(lines)
 	end
+	show(#items.." rows read from file "..filename)
+end
 
+local selectionStrategy = askForKeyword("Select objects to update by key or by proximity?", {"Key", "Proximity"})
+
+local keyColumn = "key"
+
+if fileFormat == "Excel" and selectionStrategy == "Key" then
+	keyColumn = askForKeyword("Select key column from Excel data", table.select(items[1], function(x) return x.Key end))
+end
+
+local keyProperty
+if selectionStrategy == "Key" then
+	keyProperty = askForKeyword("Select RC property to match with key", {"Name", "Code", "Id"})
+end
+
+local tolerance = 1
+if selectionStrategy == "Proximity" then
+	tolerance = askForDouble("Input the radius around data points to be searched for objects")
 end
 
 
-local objTable = {}
 
 --Select object type:
 local modelObject = askForObject("Select an existing object, we will adjust objects with its RcType")
 
-local tolerance = askForDouble("Input the radius around data points to be searched for objects")
+local objectCollection
+if selectionStrategy == "Key" then
+	objectCollection = DocumentData.ObjectCollection:filter(function(o) return o.RcType == modelObject.RcType end)	
+end
 
+local objTable = {}
+
+beginUndoBufferItem()
 
 local xCaption = "X"
 local yCaption = "Y"
 local zCaption = "Z"
-local obj
-
-beginUndoBufferItem()
 
 for i, item in ipairs(items) do
 	local x = item[xCaption]
@@ -212,12 +209,36 @@ for i, item in ipairs(items) do
 
 		local p = getPoint3D(x, y, z)
 
-		local nearbyObjects, numberOfObjects = getNearbyPointObjects2D(modelObject.RcType, p, tolerance)
-		if numberOfObjects > 0 then
-			local obj = nearbyObjects[0]
+		local obj 
+		
+		if selectionStrategy == "Proximity" then
+			local nearbyObjects, numberOfObjects = getNearbyPointObjects2D(modelObject.RcType, p, tolerance)
+			if numberOfObjects > 0 then
+				obj = nearbyObjects[0]
+			else
+				writeln("Skipping row number "..tostring(i)..": No point object of type "..modelObject.RcType.." found within "..tostring(tolerance).." m of point ("..x..", "..y..").", _warning)
+			end
+		else
+			local candidates
+			if keyProperty == "Name" then
+				candidates = objectCollection:filter(function(o) return item[keyColumn] == o.name end)
+			elseif keyProperty == "Code" then
+				candidates = objectCollection:filter(function(o) return item[keyColumn] == o.code end)
+			else
+				candidates = objectCollection:filter(function(o) return item[keyColumn] == o.id end)
+			end
+			if candidates.Count > 0 then
+				obj = candidates[0]
+			else
+				writeln("Skipping row number "..tostring(i)..": No point object of type "..modelObject.RcType.." found with "..keyProperty.." "..tostring(item[keyColumn])..")", _warning)
+			end
+		
+		end
+		
+		if obj then
 			local newLinearAddress = getLinearAddress(p, obj.Alignment)
 			
-			--Delete any formulas that may stop us from moving askForObject()
+			--Delete any formulas that may stop us from moving object
 			obj.Mileage = "="
 			obj.ReferenceMileage = "="
 			obj.DistanceAlong = "="
@@ -230,27 +251,25 @@ for i, item in ipairs(items) do
 			obj.LateralOffset = newLinearAddress.LateralOffset
 			obj.LongitudinalOffset = newLinearAddress.LongitudinalOffset
 
-    	--Adjust z if needed:
-		if z then
-			obj.VerticalOffset = "=" --remove formula, if existing
-			obj.RelativeElevation = "="
-			
-			obj.VerticalOffset = newLinearAddress.VerticalOffset
-		end
-
-		table.insert(objTable, obj)
-		writeln(tostring(i)..": Adjusted object with code "..obj.code.." and id "..obj.id.." to coordinates ("..x..", "..y..(z and ", "..z or "")..")")
-
+	    	--Adjust z if needed:
+			if z then
+				obj.VerticalOffset = "=" --remove formula, if existing
+				obj.RelativeElevation = "="
+				
+				obj.VerticalOffset = newLinearAddress.VerticalOffset
+			end
+	
+			table.insert(objTable, obj)
+			writeln(tostring(i)..": Adjusted object with code "..obj.code.." and id "..obj.id.." to coordinates ("..x..", "..y..(z and ", "..z or "")..")")
 			
 		else
-			writeln("Skipping row number "..tostring(i)..": No point object of type "..modelObject.RcType.." found within "..tostring(tolerance).." m of point ("..x..", "..y..").")
 		end
    
 	else
 		if z then
-			writeln(string.format("Skipping row number %d: %s='%s' or %s='%s' or %s='%s' is not a number.", i, xCaption, tostring(x), yCaption, tostring(y), zCaption, tostring(z)))
+			writeln(string.format("Skipping row number %d: %s='%s' or %s='%s' or %s='%s' is not a number.", i, xCaption, tostring(x), yCaption, tostring(y), zCaption, tostring(z)), _warning)
 		else
-			writeln(string.format("Skipping row number %d: %s='%s' or %s='%s' is not a number.", i, xCaption, tostring(x), yCaption, tostring(y)))
+			writeln(string.format("Skipping row number %d: %s='%s' or %s='%s' is not a number.", i, xCaption, tostring(x), yCaption, tostring(y)), _warning)
 		end
 	end
 end
@@ -259,3 +278,4 @@ endUndoBufferItem()
 
 show("\n"..#objTable.." objects were adjusted.")
 setSelectionSet(objTable)
+
