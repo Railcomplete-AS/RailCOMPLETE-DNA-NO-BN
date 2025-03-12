27b599844d61a78d10a06107789d52987c1b7e0e
khhelland <knut.helland@gmail.com>
2025-03-12 15:04:41 +0100

    Revert "wip add content with json and template"
    This reverts commit 482201fb71b39d1e948e5f75bfb6bb6a4a52b8a2.

--- CreateIssueReport.bat
+++ CreateIssueReport.bat
@@ -1,4 +1,2 @@
-
-gh search prs --author=@me --json createdAt --template '{{range .}}{{timefmt "2006/1/1" .createdAt | tablerow}}{{end}}'
 PowerShell -Command "&{ gh issue list --limit 100 --state all | foreach-object {$_ -replace(\"\t\",\";\")} | Out-File -FilePath \"..\NO-BN_issues.csv\"}"
 pause
