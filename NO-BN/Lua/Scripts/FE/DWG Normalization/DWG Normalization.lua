b3d051e88891bbb4320202d0df4a52155fe87203
RailCOMPLETE <claus.feyling@railcomplete.com>
2026-02-08 09:21:12 +0100

    Update DWG Normalization.lua
    @WIWIJ:
    CLFEY har skrevet om "staffasjen" slik det er ønskelig at alle script er, som blir releaset offisielt.
    
    - Vindusbasert tekst-dialog
    - Først vindu med "Greeting + versjon"
    -Deretter et vindu med kommandovalg, der HELP og TERMINATE er nest siste og siste valg
    -Deretter et vindu som introduserer hver aksjon eller gurppe av aksjoner som brukeren må gjøre (fordi Windows vinduet for valg av filn eller katalog har tendens til å dukke opp på feil skjerm, under et annet vindu osv, og brukeren skjønner dermed ikke hva som skal gjøres).
    - Konsistent skriving til logvindu. (Problem; lib2.writeln() legger til en ekstra newline - bruk write() inntil videre)
    - lib2,show(t, h, s) viser både tekst (på skjerm i popupvindu og i loggen), vindus-header h = "Keyword" og symbol s = nil). SYmbol er en av _ok, _warning, _error (eller nil). De tre non-nil oversettes av RC til farge hvit, gul/oransje eller grønn i loggen.
    
    - Fast header _HEADER_ vises i alle vinduer
    - Annonsering av hva som skal gjøres
    - Fortløpende logging og opptelling av det som gjøres
    -Oppsummering til sist
    - Kvitteringsvindu "Terminated" når scriptet ender på normert vis.
    - lib2.stop() benyttes dersom noe er riv ruskende galt, dette stanser scriptet og viser Lua linjenummer i log og i debugger.

--- NO-BN/Lua/Scripts/FE/DWG Normalization/DWG Normalization.lua
+++ NO-BN/Lua/Scripts/FE/DWG Normalization/DWG Normalization.lua
@@ -1,6 +1,30 @@
 --[[
+	2025-12-15 v1.0 WIWIJ Created.
+	2026-02-08 v1.1 CLFEY Added windows and usage msg etc, using lib1 and lib2 functions.
+--]]
 
-This script normalizes an AutoCAD DWG file to a consistent drafting and unit configuration. This includes:
+--FUNCTIONS--
+lib1 = includeLuaFile("Lua\\Functions\\lib1.lua") --Common Lua functions
+lib2 = includeLuaFile("Lua\\Functions\\lib2.lua") --Scripting-only Lua functions
+
+local language = lib2._DNA_COUNTRY_
+
+--CONSTANTS--
+local _HEADER_ = "DWG Normalization"
+local _VERSION_ = "1.1"
+
+local _NORMALIZE_ = "Normalize 3D geometry files"
+local _HELP_ = "Help"
+local _TERMINATE_ = "Terminate"
+
+local usageMsg = [[
+This script normalizes an AutoCAD DWG file to a consistent drafting and unit configuration for use with RailCOMPLETE 3D libraries.
+
+Open your scripting log window before running this script to keep track of progress.
+
+Version ]].._VERSION_
+
+local helpMsg = [[
 - Disables object snaps and snap mode.
 - Forces absolute coordinate input behavior.
 - Disables grid and ortho mode.
@@ -11,31 +35,65 @@ This script normalizes an AutoCAD DWG file to a consistent drafting and unit con
 - Zooms the active viewport to drawing extents.
 - Purges the .DWG file
 
---]]
+Actual script contents:
 
-write("Choose the folder containing the 3D-geometries")
-folder = askForFolderName("Choose the folder containing the 3D-geometries")
-write("Folder: "..folder.."\n")
-files = getFilesInFolder(folder, "*.dwg")
-for i = 0, files.count -1 do
-	file = files[i]:gsub("\\", "\\\\")
-	write("File: "..file.."\n")
-	
-	runCommand('(vla-activate (vla-open (vla-get-documents (vlax-get-acad-object)) "'..file..'" :vlax-false)) ')
-	
-	runCommand('(command "._SNAP" 1.0 "._OSNAP" "OFF" "._OSNAPCOORD" 1 "._SNAPMODE" 0 "._GRID" "OFF") ')
-	runCommand('(command "._UNITS" 2 3 1 3 0 "_NO" "._LUNITS" 2 "._LUPREC" 3 "._AUNITS" 0 "._AUPREC" 3 "._INSUNITS" 6 "._LIGHTINGUNITS" 2 ) ')
-	runCommand('(command "._DIMZIN" 8 "._OSMODE" 0 "._COORDS" 1 "._PICKBOX" 5 "._DYNPICOORDS" 1 "._DYNPIFORMAT" 1 "._ORTHOMODE" 0 "._PICKFIRST" 1 "._PICKADD" 0 "._ATTREQ" 0 "._ATTDIA" 0 "._FILEDIA" 1 ) ')
-	runCommand('(command "._ZOOM" "_EXTENTS") ')
-	runCommand('(command "._PLINE" "0,0" "_WIDTH" 0 0 "") ')
-	runCommand('(command "._PURGE" "ALL" "" "NO") ')
-	
-	
-	
-	write("Units changed..\n")
-	
-	-- save and close file
-	runCommand("qsave ")
-	runCommand("close ")
-end
+runCommand('(vla-activate (vla-open (vla-get-documents (vlax-get-acad-object)) "'..file..'" :vlax-false)) ')
+runCommand('(command "._SNAP" 1.0 "._OSNAP" "OFF" "._OSNAPCOORD" 1 "._SNAPMODE" 0 "._GRID" "OFF") ')
+runCommand('(command "._UNITS" 2 3 1 3 0 "_NO" "._LUNITS" 2 "._LUPREC" 3 "._AUNITS" 0 "._AUPREC" 3 "._INSUNITS" 6 "._LIGHTINGUNITS" 2 ) ')
+runCommand('(command "._DIMZIN" 8 "._OSMODE" 0 "._COORDS" 1 "._PICKBOX" 5 "._DYNPICOORDS" 1 "._DYNPIFORMAT" 1 "._ORTHOMODE" 0 "._PICKFIRST" 1 "._PICKADD" 0 "._ATTREQ" 0 "._ATTDIA" 0 "._FILEDIA" 1 ) ')
+runCommand('(command "._ZOOM" "_EXTENTS") ')
+runCommand('(command "._PLINE" "0,0" "_WIDTH" 0 0 "") ')
+runCommand('(command "._PURGE" "ALL" "" "NO") ')
+]]
 
+
+--SCRIPT--
+lib2.show(usageMsg, _HEADER_)
+
+local option
+repeat
+	option = askForKeyword("Select action:", {_NORMALIZE_, _HELP_, _TERMINATE_}, _HEADER_)
+
+	if option == _HELP_ then
+		lib2.show(helpMsg, _HEADER_)
+		
+	elseif option == _NORMALIZE_ then
+		lib2.show("Choose the folder containing the 3D geometries", _HEADER_)
+		local folderName = askForFolderName("Choose the folder containing the 3D geometries")
+		local fileNames = table.where(getFilesInFolder(folderName, "*.dwg"), function (x) return x end)
+		local t
+		for _, f in pairs(fileNames) do
+			t = t and t.."\n"..f or f
+		end
+		lib2.show("Folder:\n"..folderName.."\n\nFiles:\n"..t, _HEADER_)
+		
+		local nNormalized = 0
+		for _, fileName in pairs(fileNames) do
+			local fileName = fileName:gsub("\\", "\\\\")
+			nNormalized = nNormalized + 1
+			write(string.format("File %04d: %s...", nNormalized, fileName))
+			
+			runCommand('(vla-activate (vla-open (vla-get-documents (vlax-get-acad-object)) "'..fileName..'" :vlax-false)) ')
+			
+			runCommand('(command "._SNAP" 1.0 "._OSNAP" "OFF" "._OSNAPCOORD" 1 "._SNAPMODE" 0 "._GRID" "OFF") ')
+			runCommand('(command "._UNITS" 2 3 1 3 0 "_NO" "._LUNITS" 2 "._LUPREC" 3 "._AUNITS" 0 "._AUPREC" 3 "._INSUNITS" 6 "._LIGHTINGUNITS" 2 ) ')
+			runCommand('(command "._DIMZIN" 8 "._OSMODE" 0 "._COORDS" 1 "._PICKBOX" 5 "._DYNPICOORDS" 1 "._DYNPIFORMAT" 1 "._ORTHOMODE" 0 "._PICKFIRST" 1 "._PICKADD" 0 "._ATTREQ" 0 "._ATTDIA" 0 "._FILEDIA" 1 ) ')
+			runCommand('(command "._ZOOM" "_EXTENTS") ')
+			runCommand('(command "._PLINE" "0,0" "_WIDTH" 0 0 "") ')
+			runCommand('(command "._PURGE" "ALL" "" "NO") ')
+			runCommand("qsave ")
+			runCommand("close ")
+			write("Done\n")
+		end
+		lib2.show(nNormalized.." files normalized.", _HEADER_)
+		
+	elseif option == _TERMINATE_ then
+		--Fall through
+		
+	else
+		--Provoke error and stop
+		lib2.show("Bad option ["..option.."] - contact support@railcomplete.com.", _HEADER_)
+		lib2.stop()
+	end
+until option == _TERMINATE_
+lib2.show("Terminated.", _HEADER_)
